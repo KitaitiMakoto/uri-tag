dbff564b4a9c6411b093b0d59b721cec0124eea1
KITAITI Makoto <KitaitiMakoto@gmail.com>
2014-06-08 01:51:41 +0900

    Remove test method for not defined

--- test/uri/test_uri-tag.rb
+++ test/uri/test_uri-tag.rb
@@ -128,10 +128,6 @@ class TestTag < Test::Unit::TestCase
     assert(URI.parse('tag:example.com,2000:') != URI.parse('tag:EXAMPLE.COM,2000:'))
     assert(URI.parse('tag:example.com,2000:') != URI.parse('tag:example.com,2000-01-01:'))
   end
-
-  def test_route_from
-    skip
-  end
 end
 
 end
